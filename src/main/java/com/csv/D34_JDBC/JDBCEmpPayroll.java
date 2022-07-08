3a17ee2457775baac851475ff68e9211380c1120
Arun <rarnkmrbridgelabz.com>
2022-07-09 04:24:17 +0530

    Adding UC-4

--- src/main/java/com/csv/D34_JDBC/JDBCEmpPayroll.java
+++ src/main/java/com/csv/D34_JDBC/JDBCEmpPayroll.java
@@ -2,8 +2,8 @@ package com.csv.D34_JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class JDBCEmpPayroll {
 
@@ -12,15 +12,15 @@ public class JDBCEmpPayroll {
 		try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_payroll_service",
 					"root", "9952625052");
-			Statement statement = connection.createStatement();
+			PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set Basic_pay = 3000000.00 WHERE name = 'Terisa';");
 			
-			int updatedRow = statement.executeUpdate("update employee_payroll set Basic_pay = 3000000.00 WHERE name = 'Terisa';");
+			int updatedRow = preparedStatement.executeUpdate("update employee_payroll set Basic_pay = 3500000.00 WHERE name = 'Terisa'");
 			
 			System.out.println("Changed Rows :=" + updatedRow);
 		}
 		catch (SQLException e) {
 			
-			System.out.println("****UNABLE TO CONNECT TO DATABASE****");
+			System.out.println("****UNABLE TO CONNECT AND UPDATE TO DATABASE****");
 		}
 		finally {
 			connection.close();
